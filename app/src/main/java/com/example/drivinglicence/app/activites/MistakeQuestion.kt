4ddb675c48dda84aaf169e4644a04aa55bcc947d
devphu <phudz25022005@gmail.com>
2025-11-06 19:46:58 +0700

    add layout for driving

--- app/src/main/java/com/example/drivinglicence/app/activites/MistakeQuestion.kt
+++
@@ -1,8 +0,0 @@
-package com.example.drivinglicence.app.entity
-
-data class MistakeQuestion(
-    val question: String,
-    val answers: List<String>,
-    val correctAnswerIndex: Int,
-    val explanation: String
-)
