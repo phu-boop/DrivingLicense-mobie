c98a098e78f73cea4d2676a152237f9333f60f3f
devphu <phudz25022005@gmail.com>
2025-11-07 08:58:22 +0700

    fix bug

--- app/src/main/java/com/example/drivinglicence/app/adapter/AnswerCountDownAdapter.kt
+++ app/src/main/java/com/example/drivinglicence/app/adapter/AnswerCountDownAdapter.kt
@@ -11,16 +11,7 @@ import com.example.drivinglicence.databinding.ItemAnswerBinding
 class AnswerCountDownAdapter : BaseRecyclerViewAdapter<Answer, ItemAnswerBinding>() {
 
     @SuppressLint("SetTextI18n")
-    override fun bindData(binding: ItemAnswerBinding, item: Answer, position: Int) {
-        binding.textAnswerCount.text = "${position + 1}"
-        binding.textAnswerContent.text = item.answerContent
-        binding.textAnswerContent.setTextColor(Color.rgb(0, 0, 0))
-        binding.textAnswerCount.setTextColor(Color.rgb(0, 0, 0))
-        binding.textAnswerCount.setBackgroundResource(R.drawable.bg_round_count)
-        binding.imageStatus.visibility = View.GONE
-        /** flag
-         * 1: black
-         * 2: green
+  
          * 3: red
          * */
         when (item.flag) {
