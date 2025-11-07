63a5a7b31d1869837e7328a0914a8d6bf93fb4db
devphu <phudz25022005@gmail.com>
2025-11-07 23:36:55 +0700

    complete project

--- app/src/main/java/com/example/drivinglicence/component/activity/ActivityResultObservable.kt
+++
@@ -1,6 +0,0 @@
-package com.mobile.mbccs.base.component.activity
-
-interface ActivityResultObservable {
-    fun addObserver(activityResultObserver: ActivityResultObserver)
-    fun removeObserver(activityResultObserver: ActivityResultObserver)
-}
--- app/src/main/java/com/example/drivinglicence/component/activity/ActivityResultObserver.kt
+++
@@ -1,7 +0,0 @@
-package com.mobile.mbccs.base.component.activity
-
-import android.content.Intent
-
-interface ActivityResultObserver {
-    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?)
-}
